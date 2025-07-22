59f68966cc509e3437bcb8b8bcf9df7ca004e61a
RadhenkovaKristina <kristina.radhenkova.ru@gmail.com>
2025-07-22 14:32:19 +0300

    first try

---
+++ hw-4.js
@@ -0,0 +1,49 @@
+//1 задание
+let c = 10;
+alert(c);
+c = 20;
+alert(c);
+// 2 задание
+let iphone = 2007;
+alert(`Год выпуска первого iphone: ` + iphone);
+// 3 задание
+let creator = "Брендан Эйх"
+alert(`Создатель JS: ` + creator);
+// 4 задание
+let num1 = 10;
+let num2 = 2;
+let summ = num1 + num2;
+let razn = num1 - num2;
+let umnojenie = num1 * num2;
+let delenie = num1 / num2;
+alert(`Сумма: ` + summ + `\n` + `Разность: ` + razn + `\n` + `Произведение: ` + umnojenie + `\n` + `Частное: ` + delenie);
+// 5 задание
+let num = 2;
+let rezult = 2**5;
+alert(`Результат возведения в степень: ` + rezult);
+// 6 задание
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert(`Остаток от деления: ` + remainder);
+// 7 задание
+num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+// 8 задание
+let age = prompt("Сколько вам лет?");
+alert(`Вам ${age} лет`);
+// 9 задание
+const user = {
+   name: 'Bob',
+   age: 32,
+   isAdmin: true,
+};
+// 10 задание
+let name = prompt("Как вас зовут?");
+alert(`Привет, ${name}!`);
