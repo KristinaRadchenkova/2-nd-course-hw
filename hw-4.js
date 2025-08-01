ca73f2309c4b2b2c2ae350bd44139eb8a5639c03
RadhenkovaKristina <kristina.radhenkova.ru@gmail.com>
2025-08-01 16:00:24 +0300

    dz___3

--- hw-4.js
+++ hw-4.js
@@ -163,9 +163,9 @@ for (let number = 7; number <= 22; number++) {
 
 // 4 задание
 const obj = {
-   "Коля": '200',
-   "Вася": '300',
-   "Петя": '400'
+   Коля: '200',
+   Вася: '300',
+   Петя: '400'
 };
 
 for (let key in obj) {
@@ -175,9 +175,14 @@ for (let key in obj) {
 }
 
 // 5 задание
+let num = 0;
 for (let number = 1000; number > 50; number /= 2) {
  alert(number);
+ num ++;
 }
+ alert(num);
+
+
 
 // 6 задание
 let friday = 4;
