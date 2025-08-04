1294d06f96e5df59dec36e6f2959070d503d29f4
RadhenkovaKristina <kristina.radhenkova.ru@gmail.com>
2025-08-05 00:16:04 +0300

    dz__4

--- hw-4.js
+++ hw-4.js
@@ -147,45 +147,57 @@ switch (monthNumber) {
 
 // 1 задание
 
-for (let hello = 1; hello < 3; hello++) {
-   alert('Привет');
+// for (let hello = 1; hello < 3; hello++) {
+//    alert('Привет');
+// }
+
+// // 2 задание
+// for (let number = 1; number <= 5; number++) {
+//    alert(number);
+// }
+
+// // 3 задание
+// for (let number = 7; number <= 22; number++) {
+//    alert(number);
+// }
+
+// // 4 задание
+// const obj = {
+//    Коля: '200',
+//    Вася: '300',
+//    Петя: '400'
+// };
+
+// for (let key in obj) {
+//    if (obj.hasOwnProperty(key)) {
+//       alert(`${key} - зарплата ${obj[key]} долларов`);
+//    }
+// }
+
+// // 5 задание
+// let num = 0;
+// for (let number = 1000; number > 50; number /= 2) {
+//  alert(number);
+//  num ++;
+// }
+//  alert(num);
+
+
+
+// // 6 задание
+// let friday = 4;
+// for (let friday = 4; friday <= 31; friday += 7) {
+//    alert(`Сегодня пятница, ${friday}-е число. Не забудьте подготовить еженедельный отчёт!`);
+// }
+
+
+
+
+
+
+function Nikita() {
+   let message = 3;
+   return message;
 }
-
-// 2 задание
-for (let number = 1; number <= 5; number++) {
-   alert(number);
-}
-
-// 3 задание
-for (let number = 7; number <= 22; number++) {
-   alert(number);
-}
-
-// 4 задание
-const obj = {
-   Коля: '200',
-   Вася: '300',
-   Петя: '400'
-};
-
-for (let key in obj) {
-   if (obj.hasOwnProperty(key)) {
-      alert(`${key} - зарплата ${obj[key]} долларов`);
-   }
-}
-
-// 5 задание
-let num = 0;
-for (let number = 1000; number > 50; number /= 2) {
- alert(number);
- num ++;
-}
- alert(num);
-
-
-
-// 6 задание
-let friday = 4;
-for (let friday = 4; friday <= 31; friday += 7) {
-   alert(`Сегодня пятница, ${friday}-е число. Не забудьте подготовить еженедельный отчёт!`);
-}
+let a = Nikita();
+ console.log(a);
